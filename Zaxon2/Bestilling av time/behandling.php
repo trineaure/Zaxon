e1727f76ee6aac7eac1846dc8366d53b5440a0b7
loffeyboy <alenzy772@hotmail.com>
2015-10-11 18:03:45 +0200

    bare la inn noe tull

---
+++ Zaxon2/Bestilling av time/behandling.php
@@ -0,0 +1,122 @@
+<!DOCTYPE html>
+<html lang="en">
+
+    <!-- START: Head med meta tagger ++ -->
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+        <title>Zaxon</title>
+        <link rel="stylesheet" href="../css/style.css" />
+
+    </head>
+    <!-- END -->
+
+    <!-- START: Body -->
+    <body>
+
+        <!-- START: Container (Den som holder bredden til siden) -->
+        <div class="container">
+
+
+
+            <!-- START: Navigasjon -->
+            <nav>
+                <!-- START: Header -->
+                <header>
+                    <div class="logo">
+                        <a href="../index.html"><img src="../bilder/nylogo.png" alt=""></a>
+                    </div>
+                </header>
+                <!-- END -->
+
+                <div class="nav-bar">
+
+                    <ul>
+                        <li><a href="../index.html">Home</a></li>
+                        <li><a href="../bestill.html">Bestill time</a></li>
+                        <li><a href="../prisliste.html">Prisliste</a></li>
+                        <li><a href="../oss.html">Om oss</a></li>
+                        <li><a href="../kontakt.html">Kontakt</a></li>
+                    </ul>
+                </div>
+            </nav>
+            <!-- END -->
+
+            <!-- Lager til strukturen under navigasjonen og over footer -->
+            <div id="structure">
+
+
+
+                <!-- START: Main area where content goes -->
+                <main>
+
+                    <select>
+                        <option value="Klipp">Klipp</option>
+                        <option value="klipp1">Saab</option>
+                        <option value="klipp2">Mercedes</option>
+                        <option value="klipp3">Audi</option>
+                    </select>
+                    <p>First name: <input type="text" placeholder="ex. kort"  required/></p>
+                    <select>
+                        <option value="Farge">Farge</option>
+                        <option value="Farge1">Saab</option>
+                        <option value="Farge2">Mercedes</option>
+                        <option value="Farge3">Audi</option>
+
+                    </select>
+                    <p>First name: <input type="text" placeholder="ex. Blondt"  required/></p>
+                    <select>
+                        <option value="Striper">Striper</option>
+                        <option value="Striper1">Saab</option>
+                        <option value="Striper2">Mercedes</option>
+                        <option value="Striper3">Audi</option>
+
+                    </select>
+                    <p>First name: <input type="text" placeholder="ex. Røde striper"  required/></p>
+
+                    <select>
+                        <option value="Styling">Styling</option>
+                        <option value="Styling1">Saab</option>
+                        <option value="Styling2">Mercedes</option>
+                        <option value="Styling3">Audi</option>
+                    </select>
+                    <p>First name: <input type="text" placeholder="ex. Sexy"  required/></p>
+                    <select>
+                        <option value="tilleggsinfo">tilleggsinfo</option>
+                        <option value="tilleggsinfo1">Saab</option>
+                        <option value="tilleggsinfo2">Mercedes</option>
+                        <option value="tilleggsinfo3">Audi</option>
+
+                    </select>
+                    <p>First name: <input type="text" placeholder="noe vi må ta hensyn til?"  required/></p>
+
+                </main>
+                <!-- END -->
+
+            </div>
+
+            <!-- START: Footer -->
+            <footer>
+                <div class="footer-container">
+                    <div class="footer-col">
+                        <h2>Vårt Lokale</h2>
+                        <p>Zaxon salong <br> Zaxon gate 32, 6014 Ålesund</p>
+                    </div>
+                    <div class="footer-col">
+                        <h2>Kontakt</h2>
+                        <p>tlf. 555-zaxon<br>
+                            mail:
+                            <a href="mailto:zaxon@frisor.no" target="_top">zaxon@frisor.no</a> </p>
+                    </div>
+                    <div class="footer-col">
+                        <img src="../img/icons/face.png" alt="">
+                        <img src="../img/icons/twitter.png" alt="">
+                        <img src="../img/icons/instagram.png" alt="">
+                    </div>
+                </div>
+            </footer>
+            <!-- END -->
+        </div>
+    </body>
+    <!-- END -->
+
+</html>
