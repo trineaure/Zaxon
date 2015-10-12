f28b6954dfeacec2274cc6e70e4ed5ec8ca1bfbc
loffeyboy <alenzy772@hotmail.com>
2015-10-12 08:42:32 +0200

    endring av filplasering til "bestillings_knapp.png" :dancer:

--- Zaxon2/Bestilling av time/behandling.php
+++ Zaxon2/Bestilling av time/behandling.php
@@ -48,47 +48,48 @@
 
                 <!-- START: Main area where content goes -->
                 <main>
-
+                    
                     <select>
-                        <option value="Klipp">Klipp</option>
-                        <option value="klipp1">Saab</option>
-                        <option value="klipp2">Mercedes</option>
-                        <option value="klipp3">Audi</option>
+                        
+                        <option value="Klipp">Klipp </option>
+                        <option value="klipp1">klipp1</option>
+                        <option value="klipp2">klipp2</option>
+                        <option value="klipp3">klipp3</option>
                     </select>
-                    <p>First name: <input type="text" placeholder="ex. kort"  required/></p>
+                    <input type="text" placeholder="ex. kort"  required/>
                     <select>
                         <option value="Farge">Farge</option>
-                        <option value="Farge1">Saab</option>
-                        <option value="Farge2">Mercedes</option>
-                        <option value="Farge3">Audi</option>
+                        <option value="Farge1">Farge1</option>
+                        <option value="Farge2">Farge2</option>
+                        <option value="Farge3">Farge3</option>
 
                     </select>
-                    <p>First name: <input type="text" placeholder="ex. Blondt"  required/></p>
+                    <input type="text" placeholder="ex. Blondt"  required/>
                     <select>
                         <option value="Striper">Striper</option>
-                        <option value="Striper1">Saab</option>
-                        <option value="Striper2">Mercedes</option>
-                        <option value="Striper3">Audi</option>
+                        <option value="Striper1">Striper1</option>
+                        <option value="Striper2">Striper2</option>
+                        <option value="Striper3">Striper3</option>
 
                     </select>
-                    <p>First name: <input type="text" placeholder="ex. Røde striper"  required/></p>
+                    <input type="text" placeholder="ex. Røde striper"  required/>
 
                     <select>
                         <option value="Styling">Styling</option>
-                        <option value="Styling1">Saab</option>
-                        <option value="Styling2">Mercedes</option>
-                        <option value="Styling3">Audi</option>
+                        <option value="Styling1">Styling1</option>
+                        <option value="Styling2">Styling2</option>
+                        <option value="Styling3">Styling3</option>
                     </select>
-                    <p>First name: <input type="text" placeholder="ex. Sexy"  required/></p>
+                    <input type="text" placeholder="ex. Sexy"  required/>
                     <select>
                         <option value="tilleggsinfo">tilleggsinfo</option>
-                        <option value="tilleggsinfo1">Saab</option>
-                        <option value="tilleggsinfo2">Mercedes</option>
-                        <option value="tilleggsinfo3">Audi</option>
+                        <option value="tilleggsinfo1">tilleggsinfo1</option>
+                        <option value="tilleggsinfo2">tilleggsinfo2</option>
+                        <option value="tilleggsinfo3">tilleggsinfo3</option>
 
                     </select>
-                    <p>First name: <input type="text" placeholder="noe vi må ta hensyn til?"  required/></p>
-
+                    <input type="text" placeholder="noe vi må ta hensyn til?"  required/>
+                   
                 </main>
                 <!-- END -->
 
