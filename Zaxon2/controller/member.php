9d2b6d1091d11859f4f2f76df31d6478be6a8584
loffeyboy <alenzy772@hotmail.com>
2015-10-12 10:38:37 +0200

    after success go to new site

--- Zaxon2/controller/member.php
+++ Zaxon2/controller/member.php
@@ -66,6 +66,8 @@
                             die(print_r(sqlsrv_errors(), true));
                         } else {
                             echo "success";
+                            
+                             echo "<script>window.location = 'http://localhost/github/zaxon/zaxon2/index.html'</script>";     
                         }
                     } else {
                         echo "Connection could not be established.\n";
