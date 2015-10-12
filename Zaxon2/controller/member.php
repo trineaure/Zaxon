7180a01d90b17df0b469823fe4e34a60847606bf
Trine Gaaseide Aure <trine.aure@hotmail.com>
2015-10-12 10:27:10 +0200

    rydding

--- Zaxon2/controller/member.php
+++ Zaxon2/controller/member.php
@@ -51,25 +51,24 @@
                     $conn = sqlsrv_connect($serverName, $connectionInfo);
 
                     if ($conn) {
-
                         $fName = $_POST['First_name'];
                         $lName = $_POST['Last_name'];
                         $birth = $_POST['Birth'];
 
                         $sql = "INSERT INTO admin.Member (First_name,Last_name,Birth)
-	VALUES ('$fName','$lName','$birth')";
-
-
+                        VALUES ('$fName','$lName','$birth')";
                         $q1 = sqlsrv_query($conn, $sql);
-                        if ($q1 === false) {
-                            echo "Peacelate!!";
-                            die(print_r(sqlsrv_errors(), true));
-                        } else {
-                            echo "success";
-                        }
-                    } else {
-                        echo "Connection could not be established.\n";
-                        die(print_r(sqlsrv_errors(), true));
+                             if ($q1 === false) {
+                                 echo "Peacelate!!";
+                                 die(print_r(sqlsrv_errors(), true));
+                             } 
+                             else{
+                                 echo "success";
+                             }
+                    } 
+                    else{
+                         echo "Connection could not be established.\n";
+                         die(print_r(sqlsrv_errors(), true));
                     }
 
                     sqlsrv_close($conn);
