e1ce27e086ec9571958a42498f613617eebbdf87
Trine Gaaseide Aure <trine.aure@hotmail.com>
2015-10-15 13:07:42 +0200

    fortsettelse på mvc med employee

--- Zaxon2/controller/employeeController.php
+++ Zaxon2/controller/employeeController.php
@@ -15,5 +15,13 @@ class employeeController extends tempController {
             }
         }
     
+                         $fName = $_POST['First_name'];
+                        $lName = $_POST['Last_name'];
+                        $birth = $_POST['Birth'];
+                        $Phone_Number = $_POST['Phone_Number'];
+                        $Home_Address = $_POST['Home_Address'];
+                        $Zip_Code = $_POST['Zip_Code'];
+                        $Login_Password = $_POST['Login_Password'];
+                        $confirm_password = $_POST['confirm_password'];
 }
 
--- Zaxon2/model/empolyeeModel.php
+++ Zaxon2/model/empolyeeModel.php
@@ -41,44 +41,10 @@ class empoyeeModel {
      *
      * @return bool true on success, false otherwise
      */
-    public function add($name) {
-        return $this->addStmt->execute(array("name" => $name));
+    public function add($fName, $lName, $birth, $Phone_Number, $Home_Address, $Zip_Code, $Login_Password, $confirm_password) {
+        return $this->addStmt->execute(array("First_name", "Last_name", "Birth", "Phone_Number", "Home_Address", "Zip_Code", "Login_Password"
+                                            => $fName, $lName, $birth, $Phone_Number, $Home_Address, $Zip_Code, $Login_Password ));
     }
 
-    // TODO - create additional functions for customer model here
-
-    if ($dbConn) {
-
-                        $fName = $_POST['First_name'];
-                        $lName = $_POST['Last_name'];
-                        $birth = $_POST['Birth'];
-                        $Phone_Number = $_POST['Phone_Number'];
-                        $Home_Address = $_POST['Home_Address'];
-                        $Zip_Code = $_POST['Zip_Code'];
-                        $Login_Password = $_POST['Login_Password'];
-                        $confirm_password = $_POST['confirm_password'];
-                        
-
-                        $sql = "INSERT INTO dbo.Employee(First_name,Last_name,Birth,Phone_Number,Home_Address,Zip_Code,Login_Password)
-                        VALUES ('$fName','$lName','$birth','$Phone_Number','$Home_Address','$Zip_Code','$Login_Password')";
-
-
-                    $q1 = sqlsrv_query($conn, $sql);
-                    if ($q1 === false) {
-                        //Error page
-                        echo"<script>window.location = 'http://localhost/github/zaxon/zaxon2/memberDenied.html'</script>";
-                        die(print_r(sqlsrv_errors(), true));
-                    } else {
-                        echo "Success";
-                        
-                    }
-                } else {
-                    echo "Connection could not be established.\n";
-                    die(print_r(sqlsrv_errors(), true));
-                }
-
-
-                sqlsrv_close($conn);
-                ?>
     
 }
