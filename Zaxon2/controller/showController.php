9a162218915fa7268d93ef8706eaae7eca2e6c06
Inga Lill Bjølstad <ingabjolstad@gmail.com>
2015-10-19 12:35:09 +0200

    Lagt inn slik vi kan få se alle mobilnr. skal lage for all info nå

--- Zaxon2/controller/showController.php
+++ Zaxon2/controller/showController.php
@@ -5,7 +5,7 @@ require_once("tempController.php");
 class showController extends tempController {
 
     public function show($page) {
-        if($page == "showMember")
+        if($page == "showMembers")
             {
             $this ->showMembers();
             }           
@@ -13,35 +13,87 @@ class showController extends tempController {
             {
             $this ->addMemberAction();
             
-            }
-      
+            }  
     }
      /**
       * Function that show all the members in Zaxon
       */ 
     public function showMembers() {
-        $memberModel = $GLOBALS["memberModel"];
-        
-        $members = $memberModel->getAll();
-    }
-    /**
-     * Function that shows all the employee's in Zaxon
-     */
-    public function showEmloyee() {
-    $employeeModel = $GLOBALS["employeeModel"];
-    $employee = $employeeModel->getAll();
+        $memberModel = $GLOBALS["memberModel"];   
+        $included_members = $memberModel->getAll();
+        $data = array("included_members" => $included_members);
+         return $this->render("showMembers", $data);
+//       
     }
     
-    public function showEmployee(){
-//        $employee = noe;
-//        $employee = scandir($employee,1);        
-//        print_r($employee);
-//      
-          $employeeModel = $GLOBALS["employeeModel"];
+    /**
+     * return the result array from ms sql
+     * query asking for all information stored regarding
+     * if the given phone number fails, NULL is returned.
+     */
+    function getUserInfo($Phone_Number) {
+         $memberModel = $GLOBALS["memberModel"];
+
+        $result = $memberModel->getOneByPhone();
+    
+        return $result;
         
-       $included_employee = $employeeModel->getAll();
-       foreach ($included_employee as $employee) {
-           echo "$employee\n"; 
-    } 
-}} 
+    }
+    
+    
+   
+   
+    /**
+     * getNumMembers - return the number of signed-up users
+     * of the website
+     *     
+     *  
+    public function getNumMembers() {
+    if($thi->num_members < 0) {
+        $q = "SELECT * FROM ". memberModel;
+        $result 
+    }
+      SKAL GJØRE OM I FUNKSJONEN
+      function getNumMembers(){
+      if($this->num_members < 0){
+         $q = "SELECT * FROM ".TBL_USERS;
+         $result = mysql_query($q, $this->connection);
+         $this->num_members = mysql_numrows($result);
+      }
+      return $this->num_members;
+   }
+     */
+    
+    /**
+     * Catches informatiob about details 
+     * like owner, gropu, size, time of last change etc.
+     *
+    public function catchAllInfoMembers() {
+  
+    print_r(stat('members'));
+        $memberModel = $GLOBALS["memberModel"];   
+        $resultSet = $memberModel->getAll();
+    
+        if($resultSet -> num_rows != 0){
+            while($rows = $memberModel->fetch_assoc())
+        {
+            $givenFirst_Name = rows['First_Name'];
+            $givenLastName = rows['Last_Name'];
+            $givenBirth = rows['Birth'];
+            $givenPhone_Number = rows['Phone_Number'];
+            $givenLogin_Password = rows['Login_Password'];
+         echo "<p>Name: $givenFirst_Name $givenLastName <br> $givenBirth $givenPhone_Number $givenLogin_Password";   
+                
+        }
+          
+        } 
+             
+        else {
+            echo "No result.";
+        }
+        } 
+        */
+
+
+}
 
--- Zaxon2/view/showMembers.php
+++ Zaxon2/view/showMembers.php
@@ -1,5 +1,3 @@
-
-
 <main>
     <div  id="adminMenu">
         <a href="?page=login" class="smallButton">Medlem</a>
@@ -7,7 +5,15 @@
     </div>
 
     <div id="adminMain">
- <?php $employeeModel->showMembers?>
+  <?php
+       
+    $included_members = $GLOBALS["included_members"];
+    
+  //  var_dump($included_members);
+foreach ($included_members as $member) {
+    echo "Telefon nr: ". $member['Phone_Number'] . "<br>";
+}
+          ?>
     </div>
     
 </main>
