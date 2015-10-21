dd6399936fc3fe39e529af56414df27412ee6035
loffeyboy <alenzy772@hotmail.com>
2015-10-21 14:56:17 +0200

    ikkeno

--- Zaxon2/admin/view/searchMember.php
+++ Zaxon2/admin/view/searchMember.php
@@ -6,40 +6,40 @@
     </div>
 
     <div id="adminMain">
-<?php
-        
-        <form action = '?page=showMember' method = 'GET'>
-        < center > <h1 > My Search Engine </h1 >
-        < input type = 'text' size='90' name = 'search' > 
-        </ br > 
-        </ br > 
-        < input type = 'submit' name = 'submit' value = 'Search source code' >
-        
-        < option > 10 </ option >
-        < option > 20 </ option >
-        < option > 50 </ option > 
-        </ center >
-        </ form >
-?>
+//<?php
+//        
+//        <form action = '?page=showMember' method = 'GET'>
+//        < center > <h1 > My Search Engine </h1 >
+//        < input type = 'text' size='90' name = 'search' > 
+//        </ br > 
+//        </ br > 
+//        < input type = 'submit' name = 'submit' value = 'Search source code' >
+//        
+//        < option > 10 </ option >
+//        < option > 20 </ option >
+//        < option > 50 </ option > 
+//        </ center >
+//        </ form >
+//?>
    </div>
- <?php
-    $search_exploded = explode (" ", $search );
-    $x = 0;
-    foreach( $search_exploded as $search_each) {
-        $x++;
-        $construct = " ";
-        if( $x == 1)
-            $construct .= "keyword LIKE '%$search_each' ";
-        else 
-            $construct .= "AND keywords LIKE '%$search_each' ";
-         }
-    $construct = " SELECT * FROM Search_Enginge WHERE $construct ";
-    $run = mssql_query($construct);
-    
-    if($foundnum == 0)
-        echo " Sorry, there are no matching result for <b> $search </b>"
-            </br>
-            </br> 1. Try more general words. for example: If you want to search
-            
-    ?>
+ //<?php
+//    $search_exploded = explode (" ", $search );
+//    $x = 0;
+//    foreach( $search_exploded as $search_each) {
+//        $x++;
+//        $construct = " ";
+//        if( $x == 1)
+//            $construct .= "keyword LIKE '%$search_each' ";
+//        else 
+//            $construct .= "AND keywords LIKE '%$search_each' ";
+//         }
+//    $construct = " SELECT * FROM Search_Enginge WHERE $construct ";
+//    $run = mssql_query($construct);
+//    
+//    if($foundnum == 0)
+//        echo " Sorry, there are no matching result for <b> $search </b>"
+//            </br>
+//            </br> 1. Try more general words. for example: If you want to search
+//            
+//    ?>
 </main>
