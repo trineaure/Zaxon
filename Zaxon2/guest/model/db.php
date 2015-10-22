0abddd2049785a84f0bb990b3c6f09ecd137a867
Trine Gaaseide Aure <trine.aure@hotmail.com>
2015-10-22 12:59:57 +0200

    fiksing

--- Zaxon2/guest/model/db.php
+++ Zaxon2/guest/model/db.php
@@ -1,9 +1,11 @@
 <?php
 
 require_once("memberModel.php");
+require_once("employeeModel.php");
 
 //Create DB connection
 $dbConn = new PDO("sqlsrv:Server=$DB_HOST;Database=$DB_NAME", $DB_USER, $DB_PWD);
 
 //Create data models
 $memberModel = new memberModel($dbConn);
+$employeeModel = new employeeModel($dbConn);
