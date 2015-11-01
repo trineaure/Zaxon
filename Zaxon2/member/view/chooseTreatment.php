739012bff8cebdbdb3c54fc51b3dcaed3bfd9907
loffeyboy <alenzy772@hotmail.com>
2015-11-01 22:38:57 +0100

    porn

--- Zaxon2/member/view/chooseTreatment.php
+++ Zaxon2/member/view/chooseTreatment.php
@@ -32,6 +32,23 @@
        
     </form>
 
+<script type="text/javascript">
+        var allRadios = document.getElementsByName('treatment'); // endre på når vi kan velge flere treatments
+        var booRadio;
+        var x = 0;
+        for(x = 0; x < allRadios.length; x++){
+
+            allRadios[x].onclick = function() {
+                if(booRadio == this){
+                    this.checked = false;
+                    booRadio = null;
+                }else{
+                    booRadio = this;
+                }
+            };
+        }
+    </script>
+
 </main>
 
 
