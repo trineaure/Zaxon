4a95986790e8abb0065bedf2cf95547c92071937
Trine Gaaseide Aure <trine.aure@hotmail.com>
2015-11-02 17:13:54 +0100

    Trine and fredrik fix!
    Fikk til at footern kom under dinna time greia :ice_cream:
    ALDRI bruk inline styling! Å husk å avslutt alle tabella å div å
    paragrafa ! :)

--- Zaxon2/member/view/reservationTime.php
+++ Zaxon2/member/view/reservationTime.php
@@ -1,4 +1,36 @@
 <main>
+<script>
+     
+$(document).ready(function() {
+    updateTimes();
+    });
+     
+     function updateTimes()
+    {
+        var availableTimes = ["10:00", "10:30", "11:00", "11:30", "12:00", "12:30", "13:00", "13:30",
+                              "14:00", "14:30", "15:00", "15:30", "16:00", "16:30", "17:00", "17:30"];
+        
+        
+        <?php foreach($unavailableTimes as $tempTime): ?>
+         for(i=0;i<16;i++)
+         {
+              var x = document.getElementById(availableTimes[i]);
+              if(x.value == <?php echo date("'H:i'", strtotime($tempTime["Time_of_Day"])) ?> )
+              {
+                  var xLabel = document.getElementById('label' + x.value);
+                  xLabel.style.backgroundColor = "#ff8888";
+                  x.disabled=true;
+              }
+         }
+    
+         <?php endforeach; ?>
+        
+    }
+
+</script>
+
+
+
     <p>
 <?php
 //checks if a Member Are Logged In
@@ -39,47 +71,18 @@ if ( empty($_SESSION["MemberAreLoggedIn"]))
 </p>-->
 
 
-<script>
-     
-$(document).ready(function() {
-    updateTimes();
-    });
-     
-     function updateTimes()
-    {
-        var availableTimes = ["10:00", "10:30", "11:00", "11:30", "12:00", "12:30", "13:00", "13:30",
-                              "14:00", "14:30", "15:00", "15:30", "16:00", "16:30", "17:00", "17:30"];
-        
-        
-        <?php foreach($unavailableTimes as $tempTime): ?>
-         for(i=0;i<16;i++)
-         {
-              var x = document.getElementById(availableTimes[i]);
-              if(x.value == <?php echo date("'H:i'", strtotime($tempTime["Time_of_Day"])) ?> )
-              {
-                  var xLabel = document.getElementById('label' + x.value);
-                  xLabel.style.backgroundColor = "#ff8888";
-                  x.disabled=true;
-              }
-         }
-    
-         <?php endforeach; ?>
-        
-    }
-
-</script>
-
   
 <?php $availableTimes = array("10:00", "10:30", "11:00", "11:30", "12:00", "12:30", "13:00", "13:30",
                               "14:00", "14:30", "15:00", "15:30", "16:00", "16:30", "17:00", "17:30");?>
 
 
- <div class="timeButton">
+ 
 
    <form action="?page=reservationComplete" method="post">
                             
         <p>Velg tid</p>
-             <table style="width:10%"><p align='center'>
+        <div class="timeButton">
+             <table>
 
                   <tr>
                       <?php for($i = 0; $i <= 3; $i++) {
@@ -105,10 +108,13 @@ $(document).ready(function() {
                     <td><input type="radio" id="<?php  echo $time?>" value="<?php  echo $time?>" name="time"><label id="<?php echo "label" . $time?>" for="<?php echo $time?>"><?php echo $time ?></label></td>   
                  <?php }?>
                     </tr>
+                    </table>
+        </div>
 
         <input id="submit" type="submit" name="submit" value="Neste" />
+             
     </form>
      
- </div>
+ 
 
 </main>
