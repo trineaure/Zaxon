6241ade2590e46a12e4fbb4891a6ca3cc237bbc5
loffeyboy <alenzy772@hotmail.com>
2015-11-06 14:11:04 +0100

    bdfg

--- Zaxon2/member/view/reservationDateAndEmployee.php
+++ Zaxon2/member/view/reservationDateAndEmployee.php
@@ -3,32 +3,21 @@
     echo "Ditt MemberID er: " . $_SESSION["MembershipNumber"];
     ?>
     <div>
-        <b><p>Reserver en time</p></b>
+        <p>Reserver en time</p>
         <form method="post" action="?page=reservationTime">
 
             <?php
             $included_employee = $GLOBALS["included_employee"];
-            foreach ($included_employee as $tempEmployee) 
-                { ?>
-                    <tr>
-                        <td> <?php echo $tempEmployee["First_name"] ?> </td>
-                        <td> <input type="radio" class="regular-radio" value="<?php echo $tempEmployee["EmployeeID"]; ?>" name="givenEmployeeID">
-                            
-                            
-                            <a target="_blank">
-                                    <img src="../fellesFiler/bilder/employees/<?php echo $tempEmployee["Phone_Number"];?>.jpg" alt="Klematis" width="110" height="90">
-                                </a>
-
-                            
-                            <br>     
-                            
-<!--                            <label id="<?php echo "label" . $tempEmployee["First_name"] ?>" for="<?php echo $tempEmployee["First_name"]?>"><?php echo $tempEmployee["First_name"] ?></label></td>
-                        -->
-                        <?php
-                }
-            echo "</table>\n";?>    
-            <p><input type="text" id="datetimepicker3" placeholder="1995-06-26" name="givenReservation_date" required/></p>
-            <input id="submit" type="submit" name="submit" value="Neste" />
+            foreach ($included_employee as $tempEmployee) {
+                ?>     
+                <tr> <td>  <label<?php echo $tempEmployee["First_name"] ?> </td>  
+                   <td>  <input type="radio" class="regular-radio" value="<?php echo $tempEmployee["EmployeeID"]; ?>" name="givenEmployeeID"></label>
+                    <img src="../fellesFiler/bilder/employees/<?php echo $tempEmployee["Phone_Number"]; ?>.jpg" width="110" height="90"></td> </tr>          
+        <?php } ?>
+                
+                    </table>   
+                    <p><input type="text" id="datetimepicker3" placeholder="1995-06-26" name="givenReservation_date" required/></p>
+                    <input id="submit" type="submit" name="submit" value="Neste" />
         </form>                     
     </div>
 </main>
