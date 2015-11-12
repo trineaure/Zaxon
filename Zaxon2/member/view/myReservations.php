6843ae2645416c504f30fd05c13b7e77a93dfb3c
Trine Gaaseide Aure <trine.aure@hotmail.com>
2015-11-12 14:36:50 +0100

    :goat:

--- Zaxon2/member/view/myReservations.php
+++ Zaxon2/member/view/myReservations.php
@@ -1,51 +1,52 @@
 <main>
     <p>Her ser du en liste over dine reservasjoner.</p>
 
-    <?php   
+    <?php
     $reservations = $GLOBALS["reservations"];
-    if (isset($reservations)) { ?>
-    <table>
-       <tr>
-           <td>Dato:</td>
-           <td>Tid:</td>
-           <td>Frisør:</td>
-           <td>Behandling:</td>
-       </tr>
-    <?php  $rNr = NULL;
-        foreach($reservations as $res) { ?>
+    if (isset($reservations)) {
+        ?>
+        <table>
+            <tr>
+                <td>Dato:</td>
+                <td>Tid:</td>
+                <td>Frisør:</td>
+                <td>Behandling:</td>
+            </tr>
+            <?php
+            $rNr = NULL;
+            foreach ($reservations as $res) {
+                ?>
 
-               <?php
-               if (($res["Reservation_number"] != $rNr)||($rNr == NULL)) {?>
-                  <tr>
-                     <td> <?php echo $res["Reservation_Date"] ?> </td>
-                     <td> <?php echo "Kl. ". date("H:i", strtotime( $res["Time_of_Day"])) ?> </td>
-                     <td> <?php echo $res["First_Name"] ?> </td>
-                     <?php $rNr = $res["Reservation_number"];
-                     ?> <td> <?php
-                     } 
-               
-              if ($res["Reservation_number"] == $rNr) { 
-                  echo $res["Treatment_Name"] .", " ;
-               } 
-               else {  ?>
-                 
-                 </td> 
-                  </tr> <?php } ?>
-                  
-           
-       <?php }  ?>
-    </table> 
-          <?php }  
- 
-    else {
+        <?php if (($res["Reservation_number"] != $rNr) || ($rNr == NULL)) { ?>
+                    <tr>
+                        <td> <?php echo $res["Reservation_Date"] ?> </td>
+                        <td> <?php echo "Kl. " . date("H:i", strtotime($res["Time_of_Day"])) ?> </td>
+                        <td> <?php echo $res["First_Name"] ?> </td>
+                            <?php $rNr = $res["Reservation_number"]; ?> 
+                        <td> <?php
+                        }
+
+                        if ($res["Reservation_number"] == $rNr) {
+                            echo $res["Treatment_Name"] . ", ";
+                        } else {
+                            ?>
+                        </td> 
+                    </tr> <?php }
+                        ?>
+    <?php } ?>
+        </table> 
+
+        <?php
+    } else {
         echo "Du har ingen reservasjoner.";
-    } ?>
-            
+    }
+    ?>
+
     <div  id="big">
-         <a href="?page=order" class="button"><-Tilbake</a>
+        <a href="?page=order" class="button"><-Tilbake</a>
     </div>
 
-    
+
 </main>
 
 
