6c61125295a383eef4e57b55cc52ca5f54896a29
Trine Gaaseide Aure <trine.aure@hotmail.com>
2015-11-16 10:36:05 +0100

    bla bla

--- Zaxon2/guest/view/loginError.php
+++ Zaxon2/guest/view/loginError.php
@@ -2,6 +2,6 @@
 <p>Ditt Passord og brukernavn stemmer ikke overens.</p>
 <a href="?page=memberAdd"><p>Ingen bruker? Registrer deg her.</p></a>
              <div  id="big">
-                <a href="?page=login" class="button"><-Tilbake</a>
+                <a href="?page=login" class="button">Tilbake</a>
             </div>
 </main>
