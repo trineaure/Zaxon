705fdfbb7e43b22e534b9ed1ac35c24d7f01504d
loffeyboy <alenzy772@hotmail.com>
2015-11-16 08:36:48 +0100

    h

--- Zaxon2/master/view/reservationTreatmentFinish.php
+++ Zaxon2/master/view/reservationTreatmentFinish.php
@@ -2,7 +2,6 @@
     <div id="big">
     <p>Tusen takk for din reservasjon av en time hos oss.</p>
     <p>Ta kontakt hvis du har noen spørsmål angående behandlingen </p>
-    <p>Avbestilling osv. bla bla bla </p>
     <a href="?page=home" class="button">Gå tilbake</a>
     </div>
 </main>
